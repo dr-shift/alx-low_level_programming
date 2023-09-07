27b2df5d794860e233d9878be5efa375e62f45f4
root <root@DESKTOP-8HPPVAF.localdomain>
2023-09-07 19:20:28 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#!/bin/bash
-gcc $CFILE -o hputs
-int main()
+nclude<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
