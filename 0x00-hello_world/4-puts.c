e8dbba8cca16f1fd5abfd86945f9adfc26e4f89e
root <root@DESKTOP-8HPPVAF.localdomain>
2023-09-07 20:45:19 +0300

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
-*output without printf
-*/
-int main()
+ * 
+ *
+ * 
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
