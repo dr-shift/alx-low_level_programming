2d0445e245ce18e2acf569bd79736f2668327141
dr-shift <thetensionshift@gmail.com>
2023-09-08 19:47:08 +0300

    Mandatory

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,30 @@
+
+
+#include <stdio.h>
+
+
+/*
+*prints alphabet in reverse
+
+ */
+
+int main(void)
+
+{
+
+        char ch;
+
+
+        for (ch = 'z'; ch >= 'a'; ch--)
+
+        {
+
+                putchar(ch);
+
+        }
+
+        putchar('\n');
+
+        return (0);
+
+}
