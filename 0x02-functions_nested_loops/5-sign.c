341ec41e7ec77f2f2109bf2015d7bbf389308c97
dr-shift <thetensionshift@gmail.com>
2023-09-12 19:49:00 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,49 +1,51 @@
 #include "main.h"
 
 
-/**
 
- * print_sign - prints the sign of a number
+        /**
 
- * @n: the int to check
+         * print_sign - prints the sign of a number
 
- * Return: 1 and prints + if n is greater than zero
+         * @n: the int to check
 
- * 0 and prints 0 if n is zero
+         * Return: 1 and prints + if n is greater than zero
 
- * -1 and prints - if n is less than zero
+         * 0 and prints 0 if n is zero
 
- */
+         * -1 and prints - if n is less than zero
 
-int print_sign(int n)
+         */
 
-{
-
-        if (n > 0)
+        int print_sign(int n)
 
         {
 
-                _putchar('+');
+                if (n > 0)
 
-                return (1);
+                {
 
-        } else if (n == 0)
+                        _putchar('+');
 
-}
+                        return (1);
 
-                _putchar(48);
+                } else if (n == 0)
 
-                return (0);
+                {
 
-        } else if (n < 0)
+                        _putchar(48);
 
-        {
+                        return (0);
 
-                _putchar('-');
+                } else if (n < 0)
+
+                {
+
+                        _putchar('-');
+
+                }
+
+                        return (-1);
 
         }
 
-                return (-1);
-
-}
 
