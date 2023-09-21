44f3baeb964dfea5f0e2192bdee2c1eb72a5b4ec
dr-shift <thetensionshift@gmail.com>
2023-09-21 14:33:47 +0300

    buffer

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - Encodes a string using the rot13 algorithm.
+ * @s: The string to encode.
+ *
+ * Return: A pointer to the encoded string.
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
+
