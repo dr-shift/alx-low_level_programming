bf831bfffc8b29b467dd0dc5db6865ec821cbea9
dr-shift <thetensionshift@gmail.com>
2023-09-25 13:31:12 +0300

    correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
- * fill_memory - Fills the first n bytes of the memory area pointed to by dest with value.
- * @dest: A pointer to the memory area to fill.
- * @value: The value to fill the memory area with.
- * @n: The number of bytes to fill.
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area
+ * @b: constant byte
+ * @n: number of bytes to be filled
  *
- * Return: A pointer to the filled memory area.
+ * Return: pointer to the memory area s
  */
-char *fill_memory(char *dest, char value, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	int index = 0;
+    unsigned int i;
 
-	for (; n > 0; index++)
-	{
-		dest[index] = value;
-		n--;
-	}
+    for (i = 0; i < n; i++)
+    {
+        s[i] = b;
+    }
 
-	return (dest);
+    return (s);
 }
 
